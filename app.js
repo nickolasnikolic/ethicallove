49e15f0eb36faa6f1a3d33ba2f9e46a1bfbc3c53
Nickolas Nikolic <nickolasnikolic@gmail.com>
2016-01-30 03:51:21 -0600

    tiny commit

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require('express');
-
+var hbs = require('express-handlebars');
 
 var app = express();
+
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //var mongoose = require('mongoose');
 
 //var db = mongoose.connect( process.env.MONGOLAB_URI || 'mongodb://localhost/test' );
 
 app.get('/', function(req, res){
-    res.send('goodbye handlebars.');
+    res.render('home');
 });
 
+app.get('/admin', function(req,res){
+
+});
+
+app.post('/admin', function(req, res){
+
+});
 
 app.listen(process.env.PORT || 3000);
